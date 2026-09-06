bafef31edb8eaa49e04e0effcd0388e5d9f0b349
manot40 <kevinsandiho290100@gmail.com>
2026-09-07 00:27:27 +0700

    Feat: impl charts

--- apps/web/src/routes/records/[deviceID]/[recordTS]/+page.svelte
+++ apps/web/src/routes/records/[deviceID]/[recordTS]/+page.svelte
@@ -1,15 +1,78 @@
 <script lang="ts">
-  import { getNthLine } from '$lib/utils';
   import type { PageProps } from './$types';
+
+  import { LineChart } from 'layerchart';
+  import { curveNatural } from 'd3-shape';
+
+  import * as Card from '$components/ui/card';
+  import * as Chart from '$components/ui/chart';
 
   let { params, data }: PageProps = $props();
 
-  const recordCount = $derived((data.result.match(/\n/g) || []).length + 1);
+  const chartConfig = $derived(
+    (data.cellSeries || []).reduce((acc, next, i) => {
+      const cellNum = parseInt(next.label.match(/\d/g)?.join('') || `${i}`);
+      acc[next.key] = { label: next.label, color: `var(--cell-${cellNum}, var(--color-muted-foreground))` };
+      return acc;
+    }, {} as Chart.ChartConfig)
+  );
+
+  const series = $derived(data.cellSeries?.map((s) => ({ ...s, color: chartConfig[s.key].color })));
+  const yBaseline = $derived(data.lowestVoltage.cellVoltage ? data.lowestVoltage.cellVoltage + 0.01 : 3);
 </script>
 
-<div>{recordCount}</div>
-<div class="overflow-auto">
-  <code>{getNthLine(data.result, recordCount)}</code>
-  <div class="my-4"></div>
-  <code>{data.result}</code>
+<div class="mt-2.5 px-2.5">
+  <Card.Root>
+    <Card.Header>
+      <Card.Title>Voltage Trend</Card.Title>
+      <Card.Description>Cells voltage sag graph relative to it's power draw</Card.Description>
+    </Card.Header>
+    <Card.Content>
+      <Chart.Container config={chartConfig}>
+        <LineChart
+          data={data.cellVoltageCurrentChart}
+          {series}
+          x="current"
+          axis="x"
+          {yBaseline}
+          props={{
+            spline: { curve: curveNatural, motion: 'tween', strokeWidth: 2 },
+            highlight: { points: { r: 4 } },
+          }}>
+          {#snippet tooltip()}
+            <Chart.Tooltip hideLabel />
+          {/snippet}
+        </LineChart>
+      </Chart.Container>
+    </Card.Content>
+  </Card.Root>
 </div>
+
+<style>
+  :global(body) {
+    --cell-1: #a84c4c;
+    --cell-2: #b96a4b;
+    --cell-3: #b99c4b;
+    --cell-4: #86b94b;
+    --cell-5: #5db94b;
+    --cell-6: #4bb979;
+    --cell-7: #4bb9a1;
+    --cell-8: #4bb5b9;
+    --cell-9: #4b84b9;
+    --cell-10: #4b77b9;
+    --cell-11: #4b4db9;
+    --cell-12: #714bb9;
+    --cell-13: #9f4bb9;
+    --cell-14: #3dd137;
+    --cell-15: #c5db47;
+    --cell-16: #b94baa;
+    --cell-17: #b94b98;
+    --cell-18: #db5192;
+    --cell-19: #51a8db;
+    --cell-20: #51dbb2;
+    --cell-21: #7642ca;
+    --cell-22: #3b97ee;
+    --cell-23: #ffffff;
+    --cell-24: #8d8589;
+  }
+</style>
--- apps/web/src/routes/records/[deviceID]/[recordTS]/+page.ts
+++ apps/web/src/routes/records/[deviceID]/[recordTS]/+page.ts
@@ -1,13 +1,14 @@
 import type { PageLoad } from './$types';
+import type { RecordStatistics } from '$lib/recorder/transformer.types';
 
 import { error } from '@sveltejs/kit';
 
 export const load: PageLoad = async ({ params, fetch }) => {
-  const res = await fetch(`/records/${params.deviceID}/${params.recordTS}.replay`);
+  const res = await fetch(`/records/${params.deviceID}/${params.recordTS}.json`);
 
   if (res.ok) {
-    const result: string | null = await res.text().catch(() => null);
-    if (result) return { result };
+    const result: RecordStatistics | null = await res.json().catch(() => null);
+    if (result) return result;
   }
 
   return error(404);
